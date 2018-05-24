661c75a607d8c1d217704b8a790282befc8fcecd
Charlie Sparks <csparks@antioch.edu>
2018-05-24 16:12:09 -0700

    rendering form to add a new bookmark from the view layer

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Bookmark, PersonalBookmark
+from .forms import BookmarkForm
 
 # Create your views here.
 def index(request):
@@ -11,5 +12,7 @@ def index(request):
         context['personal_bookmarks'] = PersonalBookmark.objects.none()
     else:
         context['personal_bookmarks'] = PersonalBookmark.objects.filter(user=request.user)
+
+    context['form'] = BookmarkForm
 
     return render(request, 'bookmarks/index.html', context)
